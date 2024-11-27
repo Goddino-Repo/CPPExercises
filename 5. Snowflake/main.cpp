b0bec21885020691c420ab4dd62ff8820731d19a
Lorenzo Pellegrini <lorenzo.pellegrini@intramind-srl.com>
2024-11-27 01:03:00 +0100

    feature(Snowflake): Added snowflake exercise

---
+++ 5. Snowflake/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const string p1 = "  *  ";
+    const string p2 = " *** ";
+    const string p3 = "*****";
+
+    cout << p1 << endl;
+    cout << p2 << endl;
+    cout << p3 << endl;
+    cout << p2 << endl;
+    cout << p1 << endl;
+}
