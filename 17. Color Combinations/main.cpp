4181f8d21e9d60d287711f28293833a92624c4e7
Lorenzo Pellegrini <lorenzo.pellegrini@intramind-srl.com>
2024-11-28 12:13:03 +0100

    feature(Exercise): Added "Color Combinations" exercise

--- 17. Color Combinations/main.cpp
+++ 17. Color Combinations/main.cpp
@@ -2,21 +2,39 @@
 using namespace std;
 
 /**
- * Exercise: write a program that outputs all of the even numbers from 0 to 10 separated by spaces.
- * Use the while loop
+* Exercise: imagine you need to find out how many possible color combinations you can create with n number of colors.
+* To do this, you need to calculate the <b>factorial of n</b>, which is the product of all positive integers less than or equal to n.
+* The given code takes the n number as input.
  *
  * @return
  */
 int main() {
-    int counter = 0;
+    int numberOfColors;
 
-    while (counter <= 10) {
-        if (counter % 2 == 0) {
-            cout << counter << " ";
-        }
+    cout << "Enter the number of colors: ";
+    cin >> numberOfColors;
 
-        counter++;
+    if (numberOfColors < 0) {
+        cout << "Invalid input. Please enter a non-negative integer." << endl;
+        return 1;
     }
+
+    if (numberOfColors > 20) {
+        cout << "Factorial calculation is not supported for numbers greater than 20." << endl;
+        return 2;
+    }
+
+    if (numberOfColors == 0 || numberOfColors == 1) {
+        cout << "There is only one possible color combination for 0 or 1 colors." << endl;
+        return 0;
+    }
+
+    int factorial = 1;
+    for (int number = 1; number <= numberOfColors; number++) {
+        factorial *= number;
+    }
+
+    cout << "The number of possible color combinations is: " << factorial << endl;
 
     return 0;
 }
